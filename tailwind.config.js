7731518101b9faa836cfb9e7f9589bd2aa1389d3
isabelleliang0 <201258940@tafe.wa.edu.au>
2025-11-26 19:11:12 +0800

    feat(home): created home.html & implemented tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './*.html' 
+  ],
+  theme: {
+    extend: {      
+      colors: {
+        ivoryMist: '#F8F4F3',
+        steelBlue: '#6D9DC5',
+        dustyTaupe: '#9F8170',
+        deepMocha: '#44362F',
+      },
+    },
+  },
+  plugins: [],
+}
